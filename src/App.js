5348ecd725ec0d2b41d3ae6bd1e79f44525791cf
suedacicekli <suedacicekli@gmail.com>
2022-11-05 00:22:50 +0300

    Background color change is complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import './App.css';
-import ChangeTheme from './components/ChangeTheme';
-import Footer from './components/Footer';
 import { ThemeContextProvider } from './contexts/ThemeContext';
 import { LangContextProvider } from './contexts/LangContext';
-import ChangeLang from './components/ChangeLang';
+import Container from './Container';
 
-function App() {
-  return (
-    <div className='App'>
-      <LangContextProvider>
-        <ThemeContextProvider >
-          <ChangeTheme />
-          <hr />
-          <ChangeLang />
-          <Footer />
-        </ThemeContextProvider>
-      </LangContextProvider>
-    </div>
-  );
-}
+const App = () => (
+
+  <LangContextProvider>
+    <ThemeContextProvider >
+      <Container />
+    </ThemeContextProvider>
+  </LangContextProvider>
+);
+
 
 export default App;
---
+++ src/Container.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import ChangeTheme from './components/ChangeTheme';
+import Footer from './components/Footer';
+import ChangeLang from './components/ChangeLang';
+import ThemeContext from './contexts/ThemeContext';
+import './styles.css';
+
+function Container() {
+    const { theme } = useContext(ThemeContext);
+    console.log(theme)
+
+    return (
+        <div className={`container ${theme}`}>
+            <ChangeTheme />
+            <hr />
+            <ChangeLang />
+            <Footer />
+        </div>
+    )
+}
+
+export default Container
