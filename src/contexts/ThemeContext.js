e5d1b22255802d96dbe12068d543d5b7fd5d6e04
suedacicekli <suedacicekli@gmail.com>
2022-11-05 12:44:49 +0300

    using localStorage

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,11 +1,16 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const ThemeContext = createContext()
 
+const defaultTheme = localStorage.getItem('theme') || 'light'
 
 export const ThemeContextProvider = ({ children }) => {
 
-    const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState(defaultTheme);
+
+    useEffect(() => {
+        localStorage.setItem("theme", theme)
+    }, [theme])
 
     const toggleTheme = () => {
         setTheme((prev) => (prev === "light" ? "dark" : "light"));
